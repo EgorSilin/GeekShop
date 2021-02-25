828ec40d2dd58afed4e4fa517d4d55284eb5ed4e
Egor <egorsilin@gmail.com>
2021-02-25 13:59:17 +0500

    lesson_02: added dynamic url

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -19,7 +19,7 @@ from mainapp.views import index, products, test_context
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('products/', products),
+    path('', index, name='index'),
+    path('products/', products, name='products'),
     path('test_context/', test_context),
 ]
