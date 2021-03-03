39e12e0383fc5e24c3b102e290e36b142bd35eb6
Egor <egorsilin@gmail.com>
2021-03-03 13:27:09 +0500

    lesson_03: in urls.py added link to local MEDIA if debug=True

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainapp.views import index, products
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', products, name='products'),
 ]
+
+# check if debug on, it's mean local dev, then add path
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
