25dd9abbffbeb695b796fa912e9eb0bd3906c885
Egor <egorsilin@gmail.com>
2021-03-18 17:15:16 +0300

    lesson_07: added custom products admin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -3,4 +3,14 @@ from mainapp.models import ProductCategory, Product
 
 # Register your models here.
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+# admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'quantity', 'category')
+    fields = ('name', 'image', 'description', 'short_description', ('price', 'quantity'), 'category')
+    # readonly_fields = ('short_description',)
+    ordering = ('id',)
+    search_fields = ('name',)
+
