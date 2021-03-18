887ef48340418b5f1fdbbb74eb126b616569724e
Egor <egorsilin@gmail.com>
2021-03-18 16:50:39 +0300

    lesson_07: added users rights check with decorator

--- adminapp/views.py
+++ adminapp/views.py
@@ -2,20 +2,24 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from authapp.models import User
 from adminapp.forms import UserAdminRegistrationForm, UserAdminProfileForm
+from django.contrib.auth.decorators import user_passes_test
 
 
 # Create your views here.
+@user_passes_test(lambda u: u.is_superuser, login_url='/')
 def index(request):
     return render(request, 'adminapp/index.html')
 
 
 # READ
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users(request):
     context = {'users': User.objects.all()}
     return render(request, 'adminapp/admin-users-read.html', context)
 
 
 # CREATE
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_create(request):
     if request.method == 'POST':
         form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
@@ -29,6 +33,7 @@ def admin_users_create(request):
 
 
 # UPDATE
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_update(request, user_id):
     user = User.objects.get(id=user_id)
     if request.method == 'POST':
@@ -46,6 +51,7 @@ def admin_users_update(request, user_id):
 
 
 # DELETE
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_delete(request, user_id):
     user = User.objects.get(id=user_id)
     # user.delete()
