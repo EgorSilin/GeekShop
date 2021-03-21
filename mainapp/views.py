8ec00915dfc8f1698ca81d4bf5fef5f424ed6463
Egor <egorsilin@gmail.com>
2021-03-21 15:55:44 +0300

    lesson_08: added previous button link

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-import json
+from django.core.paginator import Paginator
 from mainapp.models import Product, ProductCategory
 
 
@@ -9,14 +9,16 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request, id=None):
+def products(request, category_id=None, page=1):
     context = {
         'title': 'GeekShop - Каталог',
         'categories': ProductCategory.objects.all(),
     }
-    context.update(
-        {
-            'products': Product.objects.filter(category_id=id) if id else Product.objects.all()
-        }
-    )
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+    paginator = Paginator(products, 3)
+    products_paginator = paginator.page(page)
+    context.update({'products': products_paginator})
     return render(request, 'mainapp/products.html', context)
